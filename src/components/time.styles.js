78d16d4df77e5394f4c24dd7e6e077fcafce451f
B Twice <54606794+bbrown2@users.noreply.github.com>
2021-05-11 18:19:41 -0400

    update style to be mobile responsive

--- src/components/time.styles.js
+++ src/components/time.styles.js
@@ -5,7 +5,8 @@ export const TimeContainer = styled(Container)`
 	height: 100vh;
     text-align: center;
 	min-width: 250px;
-
+	overflow-x: hidden;
+	padding: 0 15px;
 
 `;
 
@@ -15,45 +16,24 @@ export const ColumnContainer = styled.div`
 	align-items: center;
 	height: auto;
 	background-color: ${({ darkMode }) => (darkMode ? '#444' : '#222')};
-	padding: 25px 0;
+	padding: 10px 0;
 	min-width: 250px;
-`;
-
-export const CurrentTime = styled.h1`
-	font-size: 50px;
-	font-weight: bold;
-	padding: 0 15px;
-	color: ${({ darkMode }) => (darkMode ? '#fafafa' : null)};
 
 	@media ${device.laptop} {
-		font-size: 150px;
+		padding: 25px 0;
 	}
-`;
-
-export const Typography = styled(CurrentTime)`
-
-	font-weight: 300;
-    padding: 0 15px;
-    height: inherit;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-	width: inherit;
-
-	
-	@media ${device.laptop} {
-	font-size: 75px;
-	}
-    
-
 `;
 
 export const LeftColumn = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	align-items: flex-end;
-	width: 400px;
+	align-items: center;
+	width: auto;
+
+	@media ${device.laptop} {
+		align-items: flex-end;
+	}
 `;
 
 export const DateContainer = styled.div`
@@ -61,6 +41,10 @@ export const DateContainer = styled.div`
 		font-size: 50px;
 		font-weight: 300;
 		color: deepskyblue;
+
+		@media ${device.tablet} {
+			font-size: 70px;
+		}
 
 		@media ${device.laptop} {
 			font-size: 150px;
@@ -85,13 +69,21 @@ export const RightColumn = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	align-items: flex-start;
-	flex: 1;
-	padding: 0 50px;
+	/* flex: 1; */
+	padding: 0 10px;
 	color: #fafafa;
 
+	@media ${device.laptop} {
+		padding: 0 50px;
+	}
+
 	h4 {
-		font-size: 50px;
+		font-size: 25px;
 		font-weight: 300;
+
+		@media ${device.tablet} {
+			font-size: 50px;
+		}
 
 		@media ${device.laptop} {
 			font-size: 100px;
@@ -104,26 +96,76 @@ export const TimeContent = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	height: 250px;
+	height: 200px;
 	background-color: ${({ darkMode }) => (darkMode ? '#222' : null)};
+	width: inherit;
+	min-width: 250px;
+
+	@media ${device.tablet} {
+		flex-direction: row;
+		height: 350px;
+	}
 
 	@media ${device.laptop} {
-		flex-direction: row;
 		height: 500px;
 	}
 	/*  */
 `;
 
+export const CurrentTime = styled.h1`
+	font-size: 30px;
+	font-weight: bold;
+	padding: 0 15px;
+	color: ${({ darkMode }) => (darkMode ? '#fafafa' : null)};
+	min-width: inherit;
+
+	@media ${device.tablet} {
+		font-size: 50px;
+	}
+
+	@media ${device.laptop} {
+		font-size: 150px;
+	}
+`;
+
+export const Typography = styled(CurrentTime)`
+
+	font-weight: 300;
+    padding: 15px;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+	width: inherit;
+
+	
+	@media ${device.laptop} {
+	font-size: 75px;
+    height: inherit;
+
+	}
+    
+
+`;
+
 export const IconContainer = styled.div`
 	position: relative;
 	align-self: center;
-	font-size: 30px;
+	font-size: 15px;
 	color: deepskyblue;
-	right: 20px;
+	right: 0;
+	top: 0;
+	padding: 5px;
 	cursor: pointer;
 
 	:hover {
 		color: #fafafa;
 	}
+
+	@media ${device.laptop} {
+		font-size: 30px;
+		right: 20px;
+	}
+
 	/*  */
 `;
